13dd1392db08a9cdce0fd5a1b20e826bc54a7491
xiajunjie1 <953668865@qq.com>
2021-10-07 16:07:32 +0800

    测试

---
+++ src/review/CollectionTest.java
@@ -0,0 +1,48 @@
+package review;
+
+import java.util.*;
+
+public class CollectionTest {
+	
+	public static void main(String[] args){
+		Collection<Integer> coll=new ArrayList<Integer>();
+	Collection<Integer> temp=new ArrayList<Integer>();
+	//新增
+	coll.add(1);
+	coll.add(2);
+	coll.add(3);
+	
+	temp.add(4);
+	temp.add(5);
+	temp.add(6);
+	//一次新增一个集合的元素
+	coll.addAll(temp);
+	System.out.println(coll);
+	//删除
+	temp.remove(5);
+	System.out.println(temp);
+	//包含
+	System.out.println(coll.contains(2));
+	//是否包含该集合
+	System.out.println(coll.containsAll(temp));
+	//一般在遍历集合的时候没办法对集合进行删除操作，可以用该方法，在满足某条件的情况下，删除掉满足条件的元素
+	coll.removeIf(ele->ele>5);
+	System.out.println(coll);
+	
+	//集合的遍历
+	Iterator<Integer> it=temp.iterator();
+	while(it.hasNext()){
+		int value=it.next();
+		System.out.println(value);
+	}
+	coll.add(10);
+	coll.add(11);
+	//coll.toArray();转数组，默认为object数组
+	Integer[] a=coll.toArray(new Integer[3]);
+	System.out.println(Arrays.toString(a));
+	}
+	
+
+	
+
+}
